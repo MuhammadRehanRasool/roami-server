42540062b25bcdd0c3ae25aab0852212702d12a7
Muhammad Rehan <75864401+MuhammadRehanRasool@users.noreply.github.com>
2023-08-16 13:29:40 +0500

    Disable swagger

--- roami/urls.py
+++ roami/urls.py
@@ -6,26 +6,26 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Places Project",
-        default_version="v1",
-        description="description",
-        terms_of_service="https://your-terms-of-service.com/",
-    ),
-    public=True,
-    permission_classes=[
-        permissions.AllowAny,
-    ],
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Places Project",
+#         default_version="v1",
+#         description="description",
+#         terms_of_service="https://your-terms-of-service.com/",
+#     ),
+#     public=True,
+#     permission_classes=[
+#         permissions.AllowAny,
+#     ],
+# )
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    path(
-        "api/v1/redoc/",
-        schema_view.with_ui("redoc", cache_timeout=0),
-        name="schema-redoc",
-    ),
+    # path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    # path(
+    #     "api/v1/redoc/",
+    #     schema_view.with_ui("redoc", cache_timeout=0),
+    #     name="schema-redoc",
+    # ),
     path("accounts/", include("allauth.urls")),
     path("api/v1/", include("accounts.urls")),
     path("api/v1/about/", include("about.urls")),
